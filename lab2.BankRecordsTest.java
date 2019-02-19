a7d331c617628fcb0e9dfb6a7a25c6742e767741
strizzy008 <42658006+strizzy008@users.noreply.github.com>
2019-02-19 16:13:33 +0900

    Create lab2.BankRecordsTest.java

---
+++ lab2.BankRecordsTest.java
@@ -0,0 +1,19 @@
+package lab2;
+
+public class BankRecordsTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		// instantiating the BankRecords class as bankr
+		BankRecords bankr = new BankRecords();
+		
+		//Calling each function to perform its duty
+		bankr.readData();
+		bankr.processData();
+		bankr.printData();
+		
+	}
+
+}
