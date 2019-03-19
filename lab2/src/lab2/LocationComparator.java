11c4c591ff7be562744004792ded39a625a3a60a
bryan strawter <bstrawte@hawk.iit.edu>
2019-03-18 21:56:36 -0700

    updating code

--- lab2/src/lab2/LocationComparator.java
+++ lab2/src/lab2/LocationComparator.java
@@ -7,7 +7,26 @@ public class LocationComparator implements Comparator<BankRecords>{
 	@Override
 	public int compare(BankRecords o1, BankRecords o2) {
 	// use compareTo to compare strings
-	int result = o1.getRegion().compareTo(o2.getRegion());
-	return result;
+		int result = o1.getRegion().compareTo(o2.getRegion());
+		return result;
+	
+	
+	//String s1 = (String) o1.getRegion();
+	//String s2 = (String) o2.getRegion();
+	//double d1 = (double) o1.getIncome();
+	//double d2 = (double) o2.getIncome();
+	//int result = s1.compareTo(s2);
+	//if (result != 0) {
+		//return result;
+			//}
+	//return Double.compare(d1, d2);
+		
+		
+		
+		
+		
+	
 	}
- }
+
+
+}
--- lab2/src/lab2/Records.java
+++ lab2/src/lab2/Records.java
@@ -1,15 +1,24 @@
 package lab2;
 
+import java.util.Arrays;
+
 public class Records extends BankRecords {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		Records r = new Records();
+		//LocationComparator lc = new LocationComparator();
 		
 		r.readData();
 		r.processData();
+		//avgCalc();
+		LocationComp();
 		
+		
+		
+		
+		//lc.compare(r.getRegion(),r.getRegion());
 		// call the functions to do analytics
 		
 		//create avg(); obtain stats by location , by human, by income, by gender
@@ -19,8 +28,124 @@ public class Records extends BankRecords {
 	
 	public static void avgCalc(){
 		
-		int maleCount, femaleCount;
-		double maleIncome, FemaleIncome;
+		LocationComparator lc = new LocationComparator();
+		
+		
+	//	lc.compare(o1, o2)
+		
+		
+		int maleCount = 0, femaleCount = 0;
+		double maleIncome = 0, FemaleIncome = 0;
+		
+				
+		for(int i=0;i<robjs2.length;++i) {
+			
+			if(robjs2[i].getSex().equals("FEMALE")) {
+				
+				FemaleIncome += robjs2[i].getIncome();
+				
+				femaleCount++; }
+			
+			else
+				
+				maleCount++;
+			    maleIncome += robjs2[i].getIncome();
+		}
+		
+		
+		
+		System.out.println("Female Count = " + femaleCount + "Male Count = " + maleCount);
+					
+	}
+	
+	
+	public static void MaxMinComp() {
+		
+		double innerCityAmt =0;
+		double ruralMaxAmt =0;
+		//double
+		
+		
+		//if(robjs2[i].getRegion().equals("Innercity")) {
+			
+					
+	//	}
+	}
+		
+	private static void LocationComp() {
+		
+		Arrays.sort(robjs2,new LocationComparator());
+	            //set up needed vars for region counts & incomes per loc
+		double townCt = 0, townIncomeSum=0, townMaxIncome=0, townMinIncome=0,
+			   innerCityCt = 0, innerCityIncomeSum=0, innerCityMaxIncome=0, innerCityMinIncome=0,				
+			   suburbanCt=0, suburbanIncomeSum=0, suburbanMaxIncome=0, suburbanMinIncome=0,
+			   ruralCt=0, ruralIncomeSum=0, ruralMaxIncome=0, ruralMinIncome=0;		
+		
+		for (int i=0;i<robjs2.length;i++)
+			if (robjs2[i].getRegion().equals("RURAL")) { 
+				ruralIncomeSum += robjs2[i].getIncome();
+				++ruralCt;
+				
+				if(robjs2[i].getIncome()>ruralMaxIncome) {
+					ruralMaxIncome=robjs2[i].getIncome();					
+				}
+				
+				if(robjs2[i].getIncome()>ruralMinIncome) {
+					ruralMinIncome=robjs2[i].getIncome();					
+				}
+								
+			}
+		
+			else if(robjs2[i].getRegion().equals("INNER_CITY")) {
+				
+				innerCityIncomeSum += robjs2[i].getIncome();
+				++innerCityCt;
+				
+				if(robjs2[i].getIncome()>innerCityMaxIncome) {
+					innerCityMaxIncome=robjs2[i].getIncome();					
+				}
+				
+				if(robjs2[i].getIncome()>innerCityMinIncome) {
+					innerCityMinIncome=robjs2[i].getIncome();					
+				}
+								
+			}
+			     
+		
+			else if(robjs2[i].getRegion().equals("SUBURBAN")) {
+				
+				suburbanIncomeSum += robjs2[i].getIncome();
+				++suburbanCt;
+				
+				if(robjs2[i].getIncome()>suburbanMaxIncome) {
+					suburbanMaxIncome=robjs2[i].getIncome();					
+				}
+				
+				if(robjs2[i].getIncome()>suburbanMinIncome) {
+					suburbanMinIncome=robjs2[i].getIncome();					
+				}
+												
+			}
+		
+			else if(robjs2[i].getRegion().equals("TOWN")) { 
+				
+				townIncomeSum += robjs2[i].getIncome();
+				++townCt;
+				
+				if(robjs2[i].getIncome()>townMaxIncome) {
+					townMaxIncome=robjs2[i].getIncome();					
+				}
+				
+				if(robjs2[i].getIncome()>townMinIncome) {
+					townMinIncome=robjs2[i].getIncome();					
+				}
+			
+			}
+							
+		//setup resulting averages to print to console and to file
+		double ruralAvg = ruralIncomeSum/(ruralCt);
+		
+		
 		
 		
 	}
