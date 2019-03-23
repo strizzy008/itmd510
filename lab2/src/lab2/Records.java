781049e5b6241de612b3cfa2375d41cd0df062bc
bryan strawter <bstrawte@hawk.iit.edu>
2019-03-23 12:21:47 -0700

    updating code to 90% done with lab2

--- lab2/src/lab2/Records.java
+++ lab2/src/lab2/Records.java
@@ -1,37 +1,53 @@
+/*
+  Lab 3 is dedicated towards completing anatlyics to display and output to file a number of quireies.
+  Bryan Strawter- ITMD510
+ */
+
+
+
 package lab2;
 
 import java.text.NumberFormat;
 import java.util.Arrays;
 
+
+// creating a main class which extends Bank Records so I can utilize its classes
 public class Records extends BankRecords {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		Records r = new Records();
-		//LocationComparator lc = new LocationComparator();
 		
+		
+		// Creating an object for this current class which extends Bank Records
+		Records r = new Records();
+		
+		
+		// calling the readData function from Bank Records to collect the data in an array list
 		r.readData();
+		
+		// calling the processData function from Bank Records to place the data in an array so we can call upon them in anatlycis
 		r.processData();
-		//avgCalc();
+			
+		// displaying the analytics
+		System.out.println("\nData Analytics:");
+		
+		
+		// calling location comp to display income per region
 		LocationComp();
+		
+		// calling the MaxMinCalc function to show the highest and lowest income income per region
 		MaxMinCalc();
+		
+		// calling femsComp function to show how many females that have mortgage and savings per region
 		femsComp();
 		
-		
-		
-		
-		
-		//lc.compare(r.getRegion(),r.getRegion());
-		// call the functions to do analytics
-		
-		//create avg(); obtain stats by location , by human, by income, by gender
-			
+		// calling maleComp function to show how many males have a car and 1 child per region.
+		maleComp();
 	}
 
 	
 	
-	
+	//MaxMinCalc function to show the highest and lowest income income per region
 	public static void MaxMinCalc() {
 		
 		NumberFormat defaultFormat = NumberFormat.getCurrencyInstance();
@@ -112,37 +128,51 @@ public class Records extends BankRecords {
 					
 	}
 	
+	
+	// avgCalc class to gather the male income and the female income as well as the male count and female count
 	public static void avgCalc(){
-			
+		
+		//establishing variables
 		int maleCount = 0, femaleCount = 0;
 		double maleIncome = 0, FemaleIncome = 0;
 		
-				
+		
+		//for loop to read thru the array and store data into the variables created in this method
 		for(int i=0;i<robjs2.length;++i) {
 			
+			// if statement for getting the FEMALE Sex
 			if(robjs2[i].getSex().equals("FEMALE")) {
 				
+				//adding each female income to the variable FemaleIncome
 				FemaleIncome += robjs2[i].getIncome();
 				
+				//Increasing the female count
 				femaleCount++; }
 			
+			// if its not female then it will be a male
 			else
 				
+				// increase the male count
 				maleCount++;
+				// add to the male Income
 			    maleIncome += robjs2[i].getIncome();
 		}
 		
 		
-		
-		System.out.println("Female Count = " + femaleCount + "Male Count = " + maleCount);
-					
 	}
 	
+	// femsComp function to show how many females that have mortgage and savings per region
 	public static void femsComp() {
 		
+		
+		// creating the region counts variable
 		int ruralFmCt=0, innerCityFmCt=0, suburbanFmCt=0, townFmCt=0; 
 
+		// for loop to go thru the array and an if statement to filter on Each Region and SAVE == YES
+		// as well as a filter for sex == FEMALE and mortgage == YES
 		for (int i=0;i<robjs2.length;i++) {
+			
+			// if statement to filter on RURAL and SAVE == YES
 			if (robjs2[i].getRegion().equals("RURAL") && robjs2[i].getSave_act().equals("YES")) { 
 						
 				if(robjs2[i].getSex().equals("FEMALE") && robjs2[i].getMortgage().equals("YES")) {
@@ -178,23 +208,25 @@ public class Records extends BankRecords {
 				}			
 								
 		}
-								
+		
+		// Printing out the outcome of the analytics 
 		System.out.println("Innercity region females with mortgage and savings acct: " + innerCityFmCt);
 		System.out.println("Rural regrion females with mortgage and savings acct: " + ruralFmCt);
 		System.out.println("Suburban region females with mortgage and savings acct: " + suburbanFmCt);
-		System.out.println("town region females with mortgage and savings acct: " + townFmCt);
+		System.out.println("Town region females with mortgage and savings acct: " + townFmCt +"\n");
 		
 	}
 	
-			
+	// Location Comp function to calculate the average income for each region	
 	private static void LocationComp() {
 		
-		
+		// creating a Number Format object to format money 
 		NumberFormat defaultFormat = NumberFormat.getCurrencyInstance();
 		
+		// sorting the array with the location comparator class
 		Arrays.sort(robjs2,new LocationComparator());
 		
-	            //set up needed vars for region counts & incomes per loc
+	    //set up needed vars for region counts & incomes per loc
 		double townCt = 0, townIncomeSum=0,
 			   innerCityCt = 0, innerCityIncomeSum=0,	
 			   suburbanCt=0, suburbanIncomeSum=0,
@@ -202,7 +234,9 @@ public class Records extends BankRecords {
 		
 		
 	
-		
+		// for loop to go thru the array and filter on each region 
+		// and to seek the income and add it to the regionIncomeSum variable for each region
+		// as well as increase the count for each region
 		for (int i=0;i<robjs2.length;i++) {
 			if (robjs2[i].getRegion().equals("RURAL")) { 
 				
@@ -236,11 +270,14 @@ public class Records extends BankRecords {
 					
 		}
 		
+		
+		// performing analytics to gather the average income per region
 		double ruralAvg = ruralIncomeSum/(ruralCt);
 		double innerCityAvg = innerCityIncomeSum/(innerCityCt);
 		double suburbanAvg = suburbanIncomeSum/(suburbanCt);
 		double townAvg = townIncomeSum/(townCt);
 		
+		// printing out data from each region and formating the average variable into currency
 		System.out.println("\nInner City Region Income Average: " + defaultFormat.format(innerCityAvg));
 		System.out.println("Rural Region Income Average: " + defaultFormat.format(ruralAvg));
 		System.out.println("Suburban Region Income Average: " + defaultFormat.format(suburbanAvg));
@@ -249,4 +286,66 @@ public class Records extends BankRecords {
 	}
 	
 	
+	// maleComp function to show how many males have a car and 1 child per region.
+	public static void maleComp() {
+		
+		// variables for each region and their count
+		int ruralMct=0, innerCityMct=0, suburbanMct=0, townMct=0; 
+		
+		// for loop to go thru each element of the array as well as if statements to filter on each region 
+		// and if males have a car and 1 child
+		for (int i=0;i<robjs2.length;i++) {
+			if (robjs2[i].getRegion().equals("RURAL") && robjs2[i].getCar().equals("YES")) { 
+						
+				if(robjs2[i].getSex().equals("MALE") && robjs2[i].getChildren() == 1) {
+							
+					++ruralMct;					
+				}
+			}
+			
+			else if (robjs2[i].getRegion().equals("INNER_CITY") && robjs2[i].getCar().equals("YES")) { 
+				
+					if(robjs2[i].getSex().equals("MALE") && robjs2[i].getChildren() == 1) {
+								
+						++innerCityMct;
+					}
+			}
+			     
+		
+			else	if (robjs2[i].getRegion().equals("SUBURBAN") && robjs2[i].getCar().equals("YES")) { 
+				
+					if(robjs2[i].getSex().equals("MALE") && robjs2[i].getChildren() == 1) {
+						
+						++suburbanMct;
+				}
+		
+					
+			}
+					
+			
+			else if(robjs2[i].getRegion().equals("TOWN") && robjs2[i].getCar().equals("YES")) { 
+				
+				if(robjs2[i].getSex().equals("MALE") && robjs2[i].getChildren() == 1) {
+								
+						++townMct;					
+					}
+			
+				}		
+			
+		}
+		
+		// printing out outcome of analytics for each region
+		System.out.println("Innercity region males with car and 1 child: " + innerCityMct);
+		System.out.println("Rural regrion males with car and 1 child: " + ruralMct);
+		System.out.println("Suburban region males with car and 1 child: " + suburbanMct);
+		System.out.println("Town region males with car and 1 child: " + townMct +"\n");
+		
+		
+	}
+	
+	
+	
 }
+
+
+
